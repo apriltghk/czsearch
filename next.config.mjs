8c25fde7a95aa31e4b221389becc8db399e536a9
April Tsang <april@tofugear.com>
2026-06-19 14:19:13 +0800

    fix: tar: out: Cannot open: No such file or directory

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import packageJson from './package.json' with { type: 'json' }
 const { version } = packageJson
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   env: {
     APP_VERSION: version,
   },
