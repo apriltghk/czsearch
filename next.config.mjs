34651ad84240199174303b80a0d5cc22a9e74364
April Tsang <april@tofugear.com>
2026-06-19 14:24:17 +0800

    fix: next config build config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@ import packageJson from './package.json' with { type: 'json' }
 const { version } = packageJson
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Outputs a static './out' folder
+  basePath: '/czsearch', // Replace with your exact repository name
+  images: {
+    unoptimized: true, // GitHub Pages does not support dynamic image optimization
+  },
   env: {
     APP_VERSION: version,
   },
